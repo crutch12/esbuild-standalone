221c1c17c170d4af6fc102ec6bb50bb2cf126ec4
Konstantin Barabanov <criitch@yandex.ru>
2025-11-20 14:27:09 +0300

    0.0.16

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 (() => {
   globalThis['ESBUILD_STANDALONE_IS_DEV'] = false // true for local library usage
-  const esbuildStandaloneImportBasePath = globalThis['ESBUILD_STANDALONE_IS_DEV'] ? '' : 'https://unpkg.com/esbuild-standalone@0.0.15'
+  const esbuildStandaloneImportBasePath = globalThis['ESBUILD_STANDALONE_IS_DEV'] ? '' : 'https://unpkg.com/esbuild-standalone@0.0.16'
   importScripts(`${esbuildStandaloneImportBasePath}/lib/sw-tools/index.js`)
 })()
